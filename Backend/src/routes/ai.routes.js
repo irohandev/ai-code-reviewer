6701f24f1209aab847c4e8601cd42880cfd14773
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2025-03-02 21:15:00 +0530

    System instructions added and backend development done !

--- Backend/src/routes/ai.routes.js
+++ Backend/src/routes/ai.routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const aiController = require("../controllers/ai.controller")
 
-router.get("/get-response", aiController.getResponse)
+router.post("/get-review", aiController.getReview)
 
 
 module.exports = router
