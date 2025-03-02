e58e9e0a6f2ca67e22a71dbfbc6b198ae83a1509
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2025-03-02 17:56:35 +0530

    Ai integrated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,9 +1,13 @@
 const express = require('express') 
- const app = express()
+const app = express()
+const aiRoutes = require("./routes/ai.routes")
+
 
  app.get('/', (req, res) => {
     res.send("Hello World");
  })
+
+ app.use("/ai", aiRoutes)
 
 
  module.exports = app
