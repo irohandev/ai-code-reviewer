fedfba5d6c2ba00c8a341202dba2757f76d30ccb
Rohan Dev Singh <108291771+irohandev@users.noreply.github.com>
2025-03-02 17:35:57 +0530

    ai key added

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -1,6 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 
-const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 const prompt = "Explain how AI works";
